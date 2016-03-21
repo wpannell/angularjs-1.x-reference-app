aec42221681b9c77798ad3820c5a56000c51b868
WIL PANNELL <wpannell@gmail.com>
2016-03-21 06:14:53 +0530

    fail test for prime factors of 2 with message “expected [] to deeply equal [ 2 ]”

--- client/app/spike/prime-factors/stack-test.spec.js
+++ client/app/spike/prime-factors/stack-test.spec.js
@@ -4,4 +4,6 @@ let primeFactorsOf = number => [];
 
 describe('prime factors', () => {
   expect(primeFactorsOf(1)).to.deep.equal([]); //null —> constant transformation
+
+  expect(primeFactorsOf(2)).to.deep.equal([2]);
 });
