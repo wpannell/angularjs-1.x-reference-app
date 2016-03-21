7ab741b7c5452074752ffa5e116515e58454b7db
WIL PANNELL <wpannell@gmail.com>
2016-03-21 07:16:34 +0530

    make test bar green by introducing duplication

--- client/app/spike/prime-factors/stack-test.spec.js
+++ client/app/spike/prime-factors/stack-test.spec.js
@@ -6,6 +6,7 @@ let primeFactorsOf = number => {
 
   if (n > 1) {
     for (; n % 2 === 0; n /= 2) factors.push(2);
+    for (; n % 3 === 0; n /= 3) factors.push(3);
   }
 
   if (n > 1) factors.push(n);
