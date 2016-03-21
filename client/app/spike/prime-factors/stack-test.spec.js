2e83ea7ac77164b84bc60085d78feb8bf84f385e
WIL PANNELL <wpannell@gmail.com>
2016-03-21 06:19:48 +0530

    make test bar green with “split flow” transformation”

--- client/app/spike/prime-factors/stack-test.spec.js
+++ client/app/spike/prime-factors/stack-test.spec.js
@@ -1,12 +1,14 @@
 import {expect} from 'chai';
 
 let primeFactorsOf = number => {
-  const primeFactors = [];
-  return primeFactors;
+  const factors = [];
+  if (number > 1) factors.push(2);
+  return factors;
 };
 
 describe('prime factors', () => {
   expect(primeFactorsOf(1)).to.deep.equal([]); //null —> constant transformation
 
-  expect(primeFactorsOf(2)).to.deep.equal([2]);
+  expect(primeFactorsOf(2)).to.deep.equal([2]); //var —> constant transformation
+                                                //split flow transformation
 });
