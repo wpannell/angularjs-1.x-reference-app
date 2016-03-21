b405132fd626b8001b8011fb978f75ec93da3dd0
WIL PANNELL <wpannell@gmail.com>
2016-03-21 06:40:32 +0530

    split flow

--- client/app/spike/prime-factors/stack-test.spec.js
+++ client/app/spike/prime-factors/stack-test.spec.js
@@ -3,6 +3,10 @@ import {expect} from 'chai';
 let primeFactorsOf = number => {
   const factors = [];
   if (number > 1) {
+    if (number % 2 === 0) {
+      factors.push(2);
+      number /= 2;
+    }
     factors.push(number);
   }
   return factors;
