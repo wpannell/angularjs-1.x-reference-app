cb804e9ebccce672c8ef6fa20de8891ebd785f93
WIL PANNELL <wil.pannell@gmail.com>
2016-05-04 13:31:17 -0700

    purge basic authentication

--- server.js
+++ server.js
@@ -1,20 +1,7 @@
-const auth = require('basic-auth');
 const express = require('express');
 const cfenv = require('cfenv');
 const app = express();
 const appEnv = cfenv.getAppEnv();
-
-app.use(function (req, res, next) {
-
-  var credentials = auth(req);
-
-  if (!credentials || credentials.name !== 'john' || credentials.pass !== 'secret') {
-    res.statusCode = 401;
-    res.setHeader('WWW-Authenticate', 'Basic realm="example"');
-    return res.end('Access denied');
-  }
-  next();
-});
 
 app.use(express.static(__dirname + '/dist'));
 
