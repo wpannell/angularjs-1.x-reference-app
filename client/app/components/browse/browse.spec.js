445ed55295f6c5a50905346dcec95bd1063676ea
Able Lincoln <gotd-us-om@Ables-Mac-mini.itbic.ford.com>
2016-05-12 15:35:31 -0400

    introduce failing test

---
+++ client/app/components/browse/browse.spec.js
@@ -0,0 +1,34 @@
+import 'script!jquery/dist/jquery';
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+
+import {describe, expect, beforeEach, afterEach, it} from '../../mocha-helper';
+import {browse} from './browse';
+
+describe('orders grid', () => {
+  let $state;
+  let $scope;
+
+  let buildTemplate = () => {
+    return angular.element('<browse></browse>');
+  };
+
+  beforeEach(window.module('ui.router'));
+  beforeEach(window.module(browse.name));
+
+  beforeEach(angular.mock.inject( ($rootScope, $compile, _$state_) => {
+    $scope = $rootScope.$new();
+    $state = _$state_;
+    $compile(buildTemplate())($scope);
+    $scope.$digest();
+  }));
+
+  describe('should have', () => {
+    it('a url called /browse', () => {
+      $state.go('browse');
+      $scope.$apply();
+      console.log($state.current.url);
+      expect($state.current.url).to.equal('/browse');
+    });
+  });
+});
