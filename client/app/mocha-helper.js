39a795550e8ff3b78cff5620261edc183929b7df
Able Lincoln <gotd-us-om@Ables-Mac-mini.itbic.ford.com>
2016-05-12 15:33:25 -0400

    configure test helper

---
+++ client/app/mocha-helper.js
@@ -0,0 +1,9 @@
+import {expect} from 'chai';
+let describe = global.describe;
+let beforeEach = global.beforeEach;
+let afterEach = global.afterEach;
+let it = global.it;
+
+export {describe, beforeEach, afterEach, expect, it};
+
+// √
