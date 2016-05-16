da4099cad968551971e00538076a23235e346875
Able Lincoln <gotd-us-om@Ables-Mac-mini.itbic.ford.com>
2016-05-16 14:16:09 -0400

    introduce browse-controller

---
+++ client/app/components/browse/browse-controller.spec.js
@@ -0,0 +1,67 @@
+import {describe, expect, beforeEach, afterEach, it} from '../../mocha-helper';
+
+function GridController(browseService) {
+  let browseVm = this;
+  browseVm.total = 0;
+  browseVm.errorMessage = '';
+
+  browseVm.updateTotal = (count) => {
+    browseVm.total = count;
+  };
+
+  browseVm.updateErrorMessage = (errorMessage) => {
+    browseVm.errorMessage = errorMessage;
+  };
+
+  browseVm.fetch = () => {
+    browseService.fetch(browseVm.updateTotal, browseVm.updateErrorMessage);
+  };
+}
+
+describe.only('grid controller', () => {
+  let controller;
+  let mockService;
+
+  beforeEach( () => {
+    mockService = {};
+    controller = new GridController(mockService);
+  });
+
+  it('initializes the total', () => {
+    expect(controller.total).to.equal(0);
+  });
+
+  it('initializes the error message', () => {
+    expect(controller.errorMessage).to.equal('');
+  });
+
+  it('calls the service successfully', (done) => {
+    mockService.fetch = (success) => {
+      expect(success).to.equal(controller.updateTotal);
+      done();
+    };
+
+    controller.fetch();
+  });
+
+  it('call the failing service', (done) => {
+    mockService.fetch = (success, failure) => {
+      expect(failure).to.equal(controller.updateErrorMessage);
+      done();
+    };
+
+    controller.fetch();
+  });
+
+  it('updates the total', () => {
+    let fakeCount = 2;
+    controller.updateTotal(fakeCount);
+    expect(controller.total).to.equal(2);
+  });
+
+  it('updates the error message', () => {
+    let fakeErrorMessage = "fail";
+    controller.updateErrorMessage(fakeErrorMessage);
+    expect(controller.errorMessage).to.equal('fail');
+  });
+});
