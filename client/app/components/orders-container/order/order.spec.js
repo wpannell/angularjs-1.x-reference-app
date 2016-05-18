241d7e408aee2cca10cd92d1c9acc2a11ebc4eb2
Able Lincoln <gotd-us-om@Ables-Mac-mini.itbic.ford.com>
2016-05-18 10:26:45 -0400

    Add canary test.

---
+++ client/app/components/orders-container/order/order.spec.js
@@ -0,0 +1,9 @@
+import 'script!jquery/dist/jquery';
+import angular from 'angular';
+import {describe, expect, beforeEach, it} from '../../../mocha-helper';
+
+describe('order component', () => {
+  it('a total', () => {
+    expect(true).to.equals(true);
+  });
+});
