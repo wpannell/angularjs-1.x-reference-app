08ef84985afe32fa773bc1076c5d7ce01e768014
Able Lincoln <gotd-us-om@Ables-Mac-mini.itbic.ford.com>
2016-05-19 09:28:51 -0400

    get rid of “expect” rash

--- client/app/test-helper.js
+++ client/app/test-helper.js
@@ -1,7 +1,8 @@
-import {expect} from 'chai';
+import chai from 'chai';
 let describe = global.describe;
 let beforeEach = global.beforeEach;
 let afterEach = global.afterEach;
+let expect = global.expect;
 let it = global.it;
 
 export {describe, beforeEach, afterEach, expect, it};
